babae816740b583197a093b4870ab65d0f754a1b
Sai Kalyan Maram <66724102+kalyansai1420@users.noreply.github.com>
2026-02-23 16:20:41 +0530

    Time: 1 ms (80.59%) | Memory: 43.8 MB (42.60%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -16,39 +16,32 @@
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> res = new ArrayList<>();
-        if(root ==null) return res;
+        if(root == null) return res;
         Deque<TreeNode> deque = new ArrayDeque<>();
         deque.push(root);
         while(!deque.isEmpty()){
             int len = deque.size();
             List<Integer> level = new ArrayList<>();
-            //current level = res.size();
             if(res.size()%2==0){
                 for(int i=0;i<len;i++){
-                    TreeNode curr = deque.pollFirst();
-                    level.add(curr.val);
-                    if(curr.left!=null){
-                        deque.addLast(curr.left);
-                    }
-                    if(curr.right!=null){
-                        deque.addLast(curr.right);
-                    }
+
+                TreeNode curr = deque.pollFirst();
+                level.add(curr.val);
+                if(curr.left!=null)deque.addLast(curr.left);
+                if(curr.right!=null)deque.addLast(curr.right);
                 }
             } else {
+
                 for(int i=0;i<len;i++){
-                    TreeNode curr = deque.pollLast();
-                    level.add(curr.val);
-                    if(curr.right!=null){
-                        deque.addFirst(curr.right);
-                    }
-                    if(curr.left!=null){
-                        deque.addFirst(curr.left);
-                    }
+
+                TreeNode curr = deque.pollLast();
+                level.add(curr.val);
+                if(curr.right!=null)deque.addFirst(curr.right);
+                if(curr.left!=null)deque.addFirst(curr.left);
                 }
             }
             res.add(level);
         }
         return res;
-
     }
 }
